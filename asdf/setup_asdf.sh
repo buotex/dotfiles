b0de2cc3e34c1fef32b50ebc28a7ffebfcae1efb
Buote Xu <mail@buotexu.de>
2021-12-05 22:29:50 +0100

    Setup stuff

--- asdf/setup_asdf.sh
+++ asdf/setup_asdf.sh
@@ -3,3 +3,6 @@ asdf plugin-add python
 asdf install direnv latest
 asdf global direnv latest
 asdf install python latest
+mkdir -p ~/.config/direnv
+mkdir -p ~/bin
+ln -sf ~/.dotfiles/asdf/direnvrc ~/.config/direnv
