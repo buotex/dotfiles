77d95a11616b085e2311f093d52791caab8e2690
Buote Xu <mail@buotexu.de>
2022-10-09 23:04:00 +0200

    Some more fallbacks

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,14 +4,21 @@ if status is-interactive
     fish_add_path "$HOME/.local/bin"
     fish_add_path "$HOME/zls"
     fish_add_path "$HOME/cargo/bin"
-    nvm use lts
+    if type -sq nvm
+        nvm use lts
+    end
     set -Ux EDITOR lvim
 if set -q KITTY_INSTALLATION_DIR
     set --global KITTY_SHELL_INTEGRATION enabled
     source "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish"
     set --prepend fish_complete_path "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_completions.d"
 end
-    zoxide init fish | source
+if type -sq direnv
+direnv hook fish | source
+end
+    if type -sq zoxide
+    	zoxide init fish | source
+    end
     if type -sq exa
         alias ls='exa' 
     end
@@ -26,4 +33,3 @@ end
 # !! Contents within this block are managed by 'conda init' !!
 # <<< conda initialize <<<
 
-direnv hook fish | source
